7304276bc871827d7545b7a58a75119c7ad63f0c
Barasa m. Adam <abarasa@BRITAMGROUP.COM>
2023-05-04 22:04:38 +0300

    Adam I_am_rich

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Center(
+        child: Text('Hello World. This is Adam Mbule. Happy to be here!!!'),
+      ),
+    ),
+  );
+}
