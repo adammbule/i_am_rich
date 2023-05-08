f4b6ea475bd1108ec7e648b1995aed064c30834b
AdamMbule <adammbule98@gmail.com>
2023-05-08 23:47:11 +0300

    mmh

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Center(
-        child: Text('Hello World. This is Adam Mbule. Happy to be here!!!'),
+        child: Text('Hello World. This is Adam Mbule. Happy to be here. I am 25 years old.'),
       ),
     ),
   ); //dartfmt did the indentation by adding commas after every widget bracket.
