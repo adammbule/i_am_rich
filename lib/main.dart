45b3cf614836310b53658171b14cf9b3522f0bba
AdamMbule <adammbule98@gmail.com>
2023-05-09 00:23:39 +0300

    Anonymous Functions

--- lib/main.dart
+++ lib/main.dart
@@ -11,3 +11,15 @@ void main() {
     ),
   ); //dartfmt did the indentation by adding commas after every widget bracket.
 }
+
+//Anonymous functions-- Don't have a name associated with it
+//The following function adds the values of two double variables.
+
+void sumVariable(){
+  //assigning anonymous function to a variable
+  var sumOfvariables = (double x, double y){
+    return x + y;
+  };
+
+  print(sumOfvariables);
+}
