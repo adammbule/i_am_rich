dc914b785041bdbecbf0e9bf72ae5cfd73fc5ac7
AdamMbule <adammbule98@gmail.com>
2023-05-09 22:00:39 +0300

    Baby steps

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const MaterialApp(
-      home: Center(
-        child: Text(
-          'Hello World. This is Adam Mbule. Happy to be here. I am 25 years old.',
-        ),
+      home: Scaffold(,
+      appBar: AppBar(
+        title: Text('I_AM_RICH'),
+        backgroundColor: Colors.red, //set the appbar background to red
       ),
     ),
+    )
   ); //dartfmt did the indentation by adding commas after every widget bracket.
 }
 
