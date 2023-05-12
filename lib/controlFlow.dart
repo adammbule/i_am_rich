ee4fc4b29c8557ab21828a2b4be7d5a50b073f72
AdamMbule <adammbule98@gmail.com>
2023-05-12 23:18:04 +0300

    Comparing Strings

--- lib/controlFlow.dart
+++ lib/controlFlow.dart
@@ -3,9 +3,15 @@ import 'package:flutter/material.dart';
 
 //Testing for equality using booleans
 //equality operator ==
-// const doesOneEqualTwo = (1==2) this will return false/no/negative
+// const doesOneEqualTwo = (1==2) this will return false/no/negative or use the != operator will return true
 //print the result of boolean
 void main(){
   const doesOneEqualTwo = (1==2);
   print(doesOneEqualTwo);
+}
+//This returns false. Used to compare strings.
+void main(){
+  const guess = 'dog';
+  const dogEqualsCat = guess == 'cat';
+  print(dogEqualsCat);
 }
